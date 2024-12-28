8e32f18419c63e67005817833a0ffa6daadb6c70
Taisei <taisei.960420@gmail.com>
2024-12-28 15:01:00 +0900

    button要素のスタイルを適用

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -8,27 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './hello.component.css'
 })
 export class HelloComponent implements OnInit {
+  // タイトルを保持する
   title: string = '';
+  // メッセージを保持する
   message: string = '';
   // 現在時刻を保持する
   now: Date = new Date();
+  // スタイルクラスを保持する
+  styleClass: string = 'alert';
 
   constructor() {
     setInterval(() => {
       // 1秒ごとにnowを更新
-      this.now = new Date();
+      // this.now = new Date();
+      // スタイルクラスを更新
+      // this.styleClass = this.styleClass === 'alert' ? 'card' : 'alert';
     }, 1000);
   }
   
   // コンポーネントの初期化 → 初期値を設定
   ngOnInit(): void {
+    // タイトルを設定 
     this.title = 'Hello-App';
+    // メッセージを設定
     this.message = 'This is a message';
+    // スタイルクラスを設定
+    this.styleClass = 'alert';
   }
 
   // 現在時刻を取得するメソッド
   today() {
-    return this.now.toLocaleDateString();
+    return this.now.toLocaleString();
   }
 }
 
