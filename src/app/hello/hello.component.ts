7d9f9de83df3ed0b64a2a2b7c82ed7e053989420
Taisei <taisei.960420@gmail.com>
2024-12-31 16:06:12 +0900

    メッセージの表示を切り替え

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,48 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-hello',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './hello.component.html',
   styleUrl: './hello.component.css',
 })
 export class HelloComponent implements OnInit {
-  title: string = '';
-  message: string = 'you type: ';
-  now: Date = new Date();
-  styleClass: string = 'alert';
-  count: number = 0;
-  input: string = '';
+  title: string = 'Hello-App';
+  message: string = '';
+  visible: boolean = true;
 
-  constructor() {
-    setInterval(() => {
-      // 1秒ごとにnowを更新
-      // this.now = new Date();
-      // スタイルクラスを更新
-      // this.styleClass = this.styleClass === 'alert' ? 'card' : 'alert';
-    }, 1000);
-  }
+  constructor() {}
 
   // コンポーネントの初期化 → 初期値を設定
-  ngOnInit(): void {
-    // this.title = 'Hello-App';
-    // this.message = 'This is a message';
-    // this.styleClass = 'alert';
-  }
-
-  // 現在時刻を取得するメソッド
-  today() {
-    return this.now.toLocaleString();
-  }
+  ngOnInit(): void {}
 
   doClick() {
-    this.message = ++this.count + '回クリックされました';
+    this.visible = !this.visible;
   }
 
-  doType(val: string) {
-    this.input = val;
-    this.message = 'you type: ' + this.input;
-  }
 }
 
