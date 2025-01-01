b74717be3c2f0fd3e17f30c73c4023bb3704f628
Taisei <taisei.960420@gmail.com>
2025-01-01 17:48:38 +0900

    ローカルテンプレート変数

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,26 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 
 @Component({
   selector: 'app-hello',
   standalone: true,
-  imports: [NgSwitch, NgSwitchCase, NgSwitchDefault],
+  imports: [CommonModule],
   templateUrl: './hello.component.html',
   styleUrl: './hello.component.css',
 })
 export class HelloComponent implements OnInit {
-  title: string = 'Hello-App';
-  message: string = 'data list';
-  switch: string = 'one';
-
   constructor() {}
 
   // コンポーネントの初期化 → 初期値を設定
   ngOnInit(): void { }
   
-  doSelect(val: string) {
-    this.switch = val;
-  }
-
 }
 
